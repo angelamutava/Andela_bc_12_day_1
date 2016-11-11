c68af52594fa3416d85d2d895ecdebf97f1ad2f2
angieMutava <mutavaangela@yahoo.com>
2016-11-11 14:27:31 +0300

    checks the divisibility of a number by 3,5,3 and 5

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,10 @@
 def fizz_buzz(argument):
+	"""
+     this is a function that checks the divisibility of number by 3,5,3 and 5
+     if number is divisible by 3 return fizz
+     if number is divisible by 5 return Buzz
+     if number is divisible by both 3 and 5 return FizzBuzz
+	"""
   if argument % 3 == 0 and argument % 5 == 0:
     return 'FizzBuzz'
   elif argument % 3 == 0:
