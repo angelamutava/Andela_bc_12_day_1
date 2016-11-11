1f93c0adf9f68e10b4bf286827eb7ff1ab57fbb6
angieMutava <mutavaangela@yahoo.com>
2016-11-11 15:40:38 +0300

    class definition to illustrate the behaviour of children as per their age

--- children.py
+++ children.py
@@ -1,1 +1,9 @@
 class Children(object):
+	"""
+	This a children's class that  determines chldren behaviours as they grow up
+    attributes :
+    firstname: tkes a childs name
+    secondname :takes the childs second name
+    
+
+	"""
